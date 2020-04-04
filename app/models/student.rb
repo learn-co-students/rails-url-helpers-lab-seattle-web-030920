39d78f3c1fc7f6336d558aa0118c75e35fbd03f7
Bakugou808 <janu.m.sung@gmail.com>
2020-04-03 17:08:42 -0700

    Janu's URL Helpers lab code

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate_student
+    if self.active == false 
+      self.active = true  
+      self.save 
+    else 
+      self.active = false
+      self.save
+    end
+  end
 end
